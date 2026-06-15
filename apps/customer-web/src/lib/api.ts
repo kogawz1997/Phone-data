199028869ec6db5576e9c2965def72fa6b3de18b
KOGA.EXE <tawepornchok@gmail.com>
2026-06-16 01:42:11 +0700

    Send customer portal requests with cookies

--- apps/customer-web/src/lib/api.ts
+++ apps/customer-web/src/lib/api.ts
@@ -3,6 +3,7 @@ export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL ?? "http://loca
 export async function api<T>(path: string, options: RequestInit = {}): Promise<T> {
   const res = await fetch(`${API_BASE_URL}${path}`, {
     ...options,
+    credentials: "include",
     headers: {
       "Content-Type": "application/json",
       ...(options.headers ?? {}),
