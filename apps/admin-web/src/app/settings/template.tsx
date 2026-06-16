e6ef1a9dd6b17ec27a2731824990f08feb5a662f
KOGA.EXE <tawepornchok@gmail.com>
2026-06-16 21:04:12 +0700

    Remove settings polish template causing mobile layout issue

--- apps/admin-web/src/app/settings/template.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import type { ReactNode } from "react";
-import SafeMockupPolish from "./safe-mockup-polish";
-import SafeMockupPolishV2 from "./safe-mockup-polish-v2";
-
-export default function SettingsTemplate({ children }: { children: ReactNode }) {
-  return <div data-settings-template="safe-mockup">{children}<SafeMockupPolish /><SafeMockupPolishV2 /></div>;
-}
